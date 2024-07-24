db86a619dd310ea63155d5020671d4a265ba6f0d
Paulus sesa <114337187+seanpaulsesa@users.noreply.github.com>
2024-07-24 05:21:25 -0700

    feat:menyelesaikan tugas SP

--- database/seeders/MahasiswaSeeder.php
+++ database/seeders/MahasiswaSeeder.php
@@ -16,7 +16,7 @@ class MahasiswaSeeder extends Seeder
         Mahasiswa::create(
             [
                 'nim' => '16219281',
-                'nama_lengkap' => 'Paul Sesa',
+                'nama_lengkap' => 'Paulus Sesa',
                 'tempat_lahir' => 'Sorong',
                 'tanggal_lahir' => now(),
                 'jenis_kelamin' => 'Pria',
@@ -24,7 +24,7 @@ class MahasiswaSeeder extends Seeder
                 'no_hp' => '082198159714',
                 'foto' => '/gambar/mahasiswa/1.jpg',
                 'alamat' => 'Jln. P3 Waena',
-                'keterangan' => 'Mahasiswa Angakatan 2020',
+                'keterangan' => 'Mahasiswa Angakatan 2018',
             ]
         );
 
@@ -32,15 +32,15 @@ class MahasiswaSeeder extends Seeder
         Mahasiswa::create(
             [
                 'nim' => '16219211',
-                'nama_lengkap' => 'Jein Sesa',
+                'nama_lengkap' => 'Paul Sesa',
                 'tempat_lahir' => 'Sorong',
                 'tanggal_lahir' => now(),
-                'jenis_kelamin' => 'Wanita',
+                'jenis_kelamin' => 'Pria',
                 'jurusan_id' => 3,
                 'no_hp' => '082198159714',
-                'foto' => '/gambar/mahasiswa/2.jpg',
+                'foto' => '/gambar/mahasiswa/2.jpeg',
                 'alamat' => 'Jln. P3 Waena',
-                'keterangan' => 'Mahasiswa Angakatan 2020',
+                'keterangan' => 'Mahasiswa Angakatan 2018',
             ]
         );
 
@@ -49,15 +49,15 @@ class MahasiswaSeeder extends Seeder
         Mahasiswa::create(
             [
                 'nim' => '16219281',
-                'nama_lengkap' => 'Samuel',
+                'nama_lengkap' => 'Pace Koding',
                 'tempat_lahir' => 'Sorong',
                 'tanggal_lahir' => now(),
                 'jenis_kelamin' => 'Pria',
                 'jurusan_id' => 2,
                 'no_hp' => '082198159714',
-                'foto' => '/gambar/mahasiswa/3.jpg',
+                'foto' => '/gambar/mahasiswa/3.png',
                 'alamat' => 'Jln. P3 Waena',
-                'keterangan' => 'Mahasiswa Angakatan 2020',
+                'keterangan' => 'Mahasiswa Angakatan 2018',
             ]
         );
 
